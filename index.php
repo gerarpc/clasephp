9bd168befbce6cbdae6fa42b5b03b7fcf862dd92
gerarpc <gerarpc@gmail.com>
2021-06-01 18:30:29 -0500

    Clase de metodos POST y GET

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+   //impresiones de texto
+    echo "<h1>Hola mundo...</h1>";
+   // variables
+    $nombre = "Gerardo Patiño";
+    $edad = 43;
+    echo '<h3>'.$nombre.' edad: '.$edad.' años</h3>';
+
+    $edad += 10;
+    echo '<h3>'.$edad.'</h3>';
+    if($edad > 17){
+        echo '<h3>Es mayor de edad</h3>';
+   }else{
+        echo '<h3>Es menor de edad</h3>';
+   }
+
+   for ($i =1; $i <=5; $i++){
+        echo '<h5>Iteracion No '.$i.'</h5>';      
+   }
+
+   //Funciones
+   function sayHi(){
+        echo '<h2>Holis</h2>';
+   }
+
+   function suma(){
+        $num1 =5;
+        $num2 =10;
+
+        return $num1 + $num2;
+   }
+   sayHi();
+   echo suma()."\n";
+
+   $array= array(
+        0 => "Jeimmy",
+        1 => "Kimberly",
+        2 => "Kenny"
+   );
+
+   echo $array[2];
+   
+   for($j = 0; $j < 3; $j++){
+        echo $array[$j]."\n";
+   }
+   ?>
+</body>
+</html>
